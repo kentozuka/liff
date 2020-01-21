45bf9e0502e48ea7d689bdbe093b7c385799b409
kento <kentozuka22@gmail.com>
2020-01-21 21:00:43 +0900

    final commit

--- dist/send.js
+++ dist/send.js
@@ -1,22 +1,46 @@
-window.onload = function(e) {
-    // sendにclickイベントを追加
-    document.getElementById('send').addEventListener('click', function() {
-      // messageの中身を取得し、sendMessagesのtextに入れる
-      var ms = document.getElementById('message').value;
+window.onload = function (e) {
+  document.getElementById('send').addEventListener('click', function () {
+    const ms = document.getElementById('ms').innerText;
+    const json = JSON.parse(ms)
+    if (json[5] == "JPY") {
+      json.push(json[2])
+      sendLine(json)
+    } else {
+      var xmlhttp = new XMLHttpRequest();
+      xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == XMLHttpRequest.DONE) {   // XMLHttpRequest.DONE == 4
+          if (xmlhttp.status == 200) {
+            const response = JSON.parse(xmlhttp.responseText)
+            const rates = response.rates
+            json.push(Math.round(json[2] / rates[json[5]] * rates["JPY"]))
+            sendLine(json)
+          }
+          else if (xmlhttp.status == 400) {
+            alert('There was an error 400');
+          }
+          else {
+            alert('something else other than 200 was returned');
+          }
+        }
+      };
+      const link = 'https://openexchangerates.org/api/latest.json?app_id=4ef4298eab904caba42d8042ca5af0ef'
+      xmlhttp.open("GET", link, true);
+      xmlhttp.send();
+    }
+    function sendLine(content) {
       liff
         .sendMessages([
           {
             type: 'text',
-            text: ms
+            text: content
           }
         ])
-        .then(function() {
-          window.alert('送信完了');
-          // aleartでOKを押すと、自動でLIFFウィンドウが閉じる
+        .then(function () {
           liff.closeWindow();
         })
-        .catch(function(error) {
+        .catch(function (error) {
           window.alert('Error sending message: ' + error);
         });
-    });
-  };
+    }
+  });
+};
